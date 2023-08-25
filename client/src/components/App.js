d1f9bb34e7f2fcad84076323eb972d1d34d8aee9
Tim Mueller <trmueller5@gmail.com>
2023-08-25 10:43:10 -0500

    create Header component in client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
-  return <h1>Phase 4 Project Client</h1>;
+  return (
+    <div>
+      <Header/>
+      <h1>Phase 4 Project Client</h1>
+    </div>
+  );
 }
 
 export default App;
