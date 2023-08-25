7e5f28c012a60440e5770717ab6b097d8d56b67d
Tim Mueller <trmueller5@gmail.com>
2023-08-25 11:00:22 -0500

    add basic seed data

--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,20 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, Book
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+        
+        print("Deleting data...")
+        Book.query.delete()
+
+        print("Creating Books...")
+        book = Book(title = "A title")
+        db.session.add(book)
+        db.session.commit()
+
+        print("Seeding done!")
