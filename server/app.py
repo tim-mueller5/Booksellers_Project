4daf31ea86f70275169a14baffde8bbbec9b0f62
Tim Mueller <trmueller5@gmail.com>
2023-08-25 10:55:14 -0500

    create first db migration, simple book model

--- server/app.py
+++ server/app.py
@@ -9,6 +9,7 @@ from flask_restful import Resource
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import Book
 
 
 # Views go here!
--- server/models.py
+++ server/models.py
@@ -3,4 +3,8 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db
 
-# Models go here!
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
